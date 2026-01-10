ef358fbc638570b83fd2a5c117e3184d439c4890
Mushfiq Rashid <mushfiqarashid@gmail.com>
2026-01-10 09:29:58 -0800

    setup appjs

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([{
+    id: 1,
+    name: "Cologne",
+    image: "./products/cologne.jpg",
+    desc: "Unisex Cologne",
+    quantity: 0,
+  }]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shop</h1>
+      {products.map((product) => (
+        <div key={product.id}>
+          <h3>{product.name}</h3>
+          <h4>{product.desc}</h4>
+          <img src={product.image} alt={product.name} />
+        </div>
+      ))}
     </div>
   );
 }
