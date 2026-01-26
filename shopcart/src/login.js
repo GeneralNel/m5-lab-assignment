760726b71c4c3bb38e1d39ea5b3d7b2058469e59
Mushfiq Rashid <mushfiqarashid@gmail.com>
2026-01-25 18:51:57 -0800

    update layout of login page

--- shopcart/src/login.js
+++ shopcart/src/login.js
@@ -1,21 +1,26 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 function Login() {
     return (
-        <div className="container mt-4" style={{ maxWidth: '400px' }}>
-            <h2 className="mb-4">Login</h2>
-            <form>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" />
-                </div>
-                <button type="submit" className="btn btn-primary">Login</button>
-            </form>
-        </div>
+        <Card className="container mt-4" style={{ maxWidth: '400px' }}>
+            <h2 className="mb-4">Sign In</h2>
+            <p className="mb-4">Please login using one of the following:</p>
+            <Card className="mb-4 p-4">
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="username" className="form-label">Name:</label>
+                        <input type="text" className="form-control" id="username" placeholder="Your Name" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email:</label>
+                        <input type="email" className="form-control" id="email" placeholder="Your Email Address" />
+                    </div>
+                    <button type="submit" className="btn btn-success">Login</button>
+                </form>
+            </Card>
+            
+        </Card>
     );
 }
 
